deb1709d059ca1376100c453929212d41f00f5d4
psjyyb <ass6728@naver.com>
2024-04-23 11:48:51 +0900

    2번째 커밋

--- parkseongjun/src/co/yedam/BoardDao.java
+++ parkseongjun/src/co/yedam/BoardDao.java
@@ -1,5 +1,5 @@
 package co.yedam;
 
 public class BoardDao {
-
+	
 }
--- parkseongjun/src/co/yedam/Report.java
+++ parkseongjun/src/co/yedam/Report.java
@@ -1,5 +1,46 @@
 package co.yedam;
 
 public class Report {
-
+	private int postNumber;
+	private int reportNumber;
+	private String reportTitle;
+	private String reportWrite;
+	private String mem_id;
+	private int report_hand;
+	public int getPostNumber() {
+		return postNumber;
+	}
+	public void setPostNumber(int postNumber) {
+		this.postNumber = postNumber;
+	}
+	public int getReportNumber() {
+		return reportNumber;
+	}
+	public void setReportNumber(int reportNumber) {
+		this.reportNumber = reportNumber;
+	}
+	public String getReportTitle() {
+		return reportTitle;
+	}
+	public void setReportTitle(String reportTitle) {
+		this.reportTitle = reportTitle;
+	}
+	public String getReportWrite() {
+		return reportWrite;
+	}
+	public void setReportWrite(String reportWrite) {
+		this.reportWrite = reportWrite;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public int getReport_hand() {
+		return report_hand;
+	}
+	public void setReport_hand(int report_hand) {
+		this.report_hand = report_hand;
+	}
 }
